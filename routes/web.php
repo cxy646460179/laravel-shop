4714be0c4452f081ec5c96080ae5131eecc184bb
xiangyu <cxy646460179@gmail.com>
2018-08-05 09:55:12 +0000

    用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ Route::get('/','PagesController@root')->name('root');
 
 Auth::routes();
 
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
+
 Route::group(['middleware' => 'auth'], function() {
     Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
